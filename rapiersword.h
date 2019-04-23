bbde2de81c822fd731d6bdcf2daa44a613fb95ce
akosuri54 <48737594+akosuri54@users.noreply.github.com>
2019-04-23 15:59:26 -0400

    Create rapiersword.h

---
+++ rapiersword.h
@@ -0,0 +1,14 @@
+#include "../Heroes/User_Character.h"
+
+#ifndef TEAMTUTOR_RAPIERSWORD_H
+#define TEAMTUTOR_RAPIERSWORD_H
+
+
+class RapierSword {
+
+public:
+    static void getRapierSword(User_Character &user);
+};
+
+
+#endif //TEAMTUTOR_RAPIERSWORD_H
