4d11a70a16626a8419dd1c4ea194019bda369c13
akosuri54 <48737594+akosuri54@users.noreply.github.com>
2019-04-23 15:55:38 -0400

    Create bowandarrow.cpp

---
+++ bowandarrow.cpp
@@ -0,0 +1,15 @@
+
+#include "bowandnarrow.h"
+#include "../Heroes/User_Character.h"
+#include <iostream>
+
+using namespace std;
+
+void BowAndArrow::getBowAndArrow(User_Character &user)
+{
+  user.setMagical (false);
+  user.addHealth(10);
+  user.addAttack(1);
+  user.addDefense(1);
+
+}
