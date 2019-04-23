2ef5a348196bc3365204d84b946a5b31c69c3956
akosuri54 <48737594+akosuri54@users.noreply.github.com>
2019-04-23 16:00:43 -0400

    Create wand.h

---
+++ wand.h
@@ -0,0 +1,14 @@
+#include "../Heroes/User_Character.h"
+
+#ifndef TEAMTUTOR_WAND_H
+#define TEAMTUTOR_WAND_H
+
+
+class Wand {
+
+public:
+    static void getWand(User_Character &user);
+};
+
+
+#endif //TEAMTUTOR_WAND_H
