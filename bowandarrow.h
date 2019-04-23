77ef04749b902b8d7e732f44aa3caeb196e7f62e
akosuri54 <48737594+akosuri54@users.noreply.github.com>
2019-04-23 15:56:59 -0400

    Create bowandarrow.h

---
+++ bowandarrow.h
@@ -0,0 +1,14 @@
+#include "../Heroes/User_Character.h"
+
+#ifndef TEAMTUTOR_BOWANDARROW_H
+#define TEAMTUTOR_BOWANDARROW_H
+
+
+class BowAndArrow {
+
+public:
+    static void getBowAndArrow(User_Character &user);
+};
+
+
+#endif //TEAMTUTOR_BOWANDARROW_H
