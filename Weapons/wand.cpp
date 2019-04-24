7a4004311c6da5917d4a1b49b51a9ed71b8cd765
akosuri54 <48737594+akosuri54@users.noreply.github.com>
2019-04-24 14:38:27 -0400

    Update wand.cpp

--- Weapons/wand.cpp
+++ Weapons/wand.cpp
@@ -7,7 +7,7 @@ using namespace std;
 void Wand::getWand(User_Character &user)
 {
   user.setMagical (true);
-  user.addHealth(0.2*user.getMaxHealth());
+  user.addHealth(30);
   user.addAttack(20);
   user.addDefense(20);
 }
